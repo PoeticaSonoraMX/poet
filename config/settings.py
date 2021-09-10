00b3a0d11f87393f25ddb5d1dda009b32e50d0da
davlum <david.lum91@gmail.com>
2021-09-10 14:55:18 -0300

    Add subpath /rda in production

--- config/settings.py
+++ config/settings.py
@@ -219,3 +219,8 @@ NOSE_ARGS = [
     '--with-coverage',
     '--cover-package=app',
 ]
+
+# To have the project at a subdirectory
+# https://stackoverflow.com/questions/28147916/how-to-host-a-django-project-in-a-subpath
+if not DEBUG:
+    FORCE_SCRIPT_NAME = '/rda'
